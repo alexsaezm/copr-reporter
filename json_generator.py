3922a503245db907e8b7dbcf195be64aa7f248a9
Tom Stellard <tstellar@redhat.com>
2022-04-20 21:47:25 -0700

    Allow specifying a copr config file in config.ini

--- json_generator.py
+++ json_generator.py
@@ -10,30 +10,31 @@ def load_config(file):
     return c
 
 
-def retrieve_packages(project_owner, project_name):
+def retrieve_packages(client, project_owner, project_name):
     c = Client.create_from_config_file()
-    p = c.package_proxy.get_list(project_owner, project_name, pagination={"order": "name"}, with_latest_build=True)
+    p = client.package_proxy.get_list(project_owner, project_name, pagination={"order": "name"}, with_latest_build=True)
     return p
 
 
-def retrieve_builds(package):
-    c = Client.create_from_config_file()
-    b = c.build_chroot_proxy.get_list(package.builds['latest']['id'])
+def retrieve_builds(client, package):
+    b = client.build_chroot_proxy.get_list(package.builds['latest']['id'])
     return b
 
 
 if __name__ == '__main__':
     config = load_config(file="./config.ini")
-    packages_a = retrieve_packages(config['current']['owner'], config['current']['project'])
-    packages_b = retrieve_packages(config['next']['owner'], config['next']['project'])
+    client_a = Client.create_from_config_file(config.get('current', 'config', fallback = None))
+    client_b = Client.create_from_config_file(config.get('next', 'config', fallback = None))
+    packages_a = retrieve_packages(client_a, config['current']['owner'], config['current']['project'])
+    packages_b = retrieve_packages(client_b, config['next']['owner'], config['next']['project'])
 
     packages = {}
     for pa, pb in zip(packages_a, packages_b):
         new_package = Munch(name=pa.name)
         new_package.builds_a = {}
         new_package.builds_b = {}
-        builds_a = retrieve_builds(pa)
-        builds_b = retrieve_builds(pb)
+        builds_a = retrieve_builds(client, pa)
+        builds_b = retrieve_builds(client, pb)
         for ba, bb in zip(builds_a, builds_b):
             # Stores the arch, the URL and the state of the build using the the chroot as a key
             arch = ba.name.split('-')[-1]
